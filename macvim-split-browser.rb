fc6576648efb84f72d6bf3bfa4897e218851ea4b
Greg Kellogg <greg@kelloggs.fm>
2014-09-07 19:13:06 -0700

    This is what I get for using TextEdit

--- macvim-split-browser.rb
+++ macvim-split-browser.rb
@@ -6,8 +6,8 @@ require 'formula'
 
 
 class MacvimSplitBrowser < Formula
-  homepage ‘https://github.com/alloy/macvim/'
-  url ‘https://github.com/gregkellogg/macvim.git'
+  homepage 'https://github.com/alloy/macvim/'
+  url 'https://github.com/gregkellogg/macvim.git'
   version “740022”
 
   #  Added patch so it will compile easily on 10.9
